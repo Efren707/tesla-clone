7b0389c61327a2f079d323ce2be0bf47554e0ae3
efren <efrenbahen@gmail.com>
2022-04-09 12:18:33 -0500

    add post controller and views

--- instagram-clone/config/initializers/carrierwave.rb
+++ instagram-clone/config/initializers/carrierwave.rb
@@ -8,5 +8,5 @@ CarrierWave.configure do |config|
     config.permissions = 0666
     config.cache_dir = "#{Rails.root}/tmp/"
     config.fog_directory = ENV['FOG_DIRECTORY']
-    config.fog_attribute = {'Cache-Control' => "max-age=#{365.day.to_i}"}
+    config.fog_attributes = {'Cache-Control' => "max-age=#{365.day.to_i}"}
 end
--- instagram-clone/config/routes.rb
+++ instagram-clone/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :accounts
+  resources :posts, only: [:show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/dashboard" => "accounts#index"
   root to: "public#homepage"
