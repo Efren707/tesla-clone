e6ed7bdc62a06a04c6d2839269ff39fd7233237a
efren <efrenbahen@gmail.com>
2022-05-19 16:02:52 -0500

    testing routing for deployed site

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Routes, Route} from 'react-router-dom';
 import Header from './components/header';
 import Home from './components/home';
 import ModelSX from './components/ModelS';
@@ -15,13 +15,12 @@ function App() {
   return (
     <div className="App">
 
-      <Router>
+      
         <Header/>
 
         <Routes>
-          
-          <Route path="/tesla-clone/" element={<Home />} />
-
+    
+          <Route path="/tesla-clone" element={<Home />} />
           <Route path="/tesla-clone/models" element={<ModelSX title={"Model S"} bgimg={modelS} range={396} zerotosixty={1.99} topSpeed={200} hp={1020} />} />
           <Route path="/tesla-clone/modelx" element={<ModelSX title={"Model X"} bgimg={modelX} range={333} zerotosixty={2.5} topSpeed={155} hp={1020} />} />
           <Route path="/tesla-clone/model3" element={<Model3Y title={"Model 3"} bgimg={model3} range={358} stat={3.1} />} />
@@ -29,7 +28,7 @@ function App() {
 
         </Routes>
       
-      </Router>
+      
 
     </div>
   );
